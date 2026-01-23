2c7c69c53807a4d344341a46a584292c77e40c43
Poorna Chandra <poornarikki@gmail.com>
2026-01-23 21:58:05 +0530

    User Request and User Response class are updated

--- user-management/src/main/java/com/example/user_management/dto/UserRequestDto.java
+++ user-management/src/main/java/com/example/user_management/dto/UserRequestDto.java
@@ -1,4 +1,40 @@
 package com.example.user_management.dto;
 
 public class UserRequestDto {
+    private String name;
+    private String email;
+    private String password;
+    private long phoneNo;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public long getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(long phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- user-management/src/main/java/com/example/user_management/dto/UserResponseDto.java
+++ user-management/src/main/java/com/example/user_management/dto/UserResponseDto.java
@@ -1,4 +1,49 @@
 package com.example.user_management.dto;
 
 public class UserResponseDto {
+    private String name;
+    private String email;
+    private long phoneNo;
+    private String role;
+    private boolean active;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public long getPhoneNo() {
+        return phoneNo;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setPhoneNo(long phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
