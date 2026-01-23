78ceb713cdf53ef5ef34159a48e8f9e612e0d798
Poorna Chandra <poornarikki@gmail.com>
2026-01-23 22:08:47 +0530

    The user details are updated

--- user-management/src/main/java/com/example/user_management/entity/User.java
+++ user-management/src/main/java/com/example/user_management/entity/User.java
@@ -1,4 +1,20 @@
 package com.example.user_management.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private long phoneNo;
+    private String password;
+    private String role="User";
+    private boolean isActive=true;
 }
--- user-management/src/main/java/com/example/user_management/repository/UserRepository.java
+++ user-management/src/main/java/com/example/user_management/repository/UserRepository.java
@@ -1,4 +1,5 @@
 package com.example.user_management.repository;
 
 public class UserRepository {
+
 }
